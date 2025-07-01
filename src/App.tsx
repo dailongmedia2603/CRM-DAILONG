78c9755ec4307abe68a7fe36ccbb3e224cc19869
[dyad] <git@dyad.sh>
2025-07-01 21:07:17 +0700

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ClientsPage from "./pages/clients/Index";
 import ProjectsPage from "./pages/projects/Index";
+import LeadsPage from "./pages/sales/leads/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/clients" element={<ClientsPage />} />
           <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/sales/leads" element={<LeadsPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
