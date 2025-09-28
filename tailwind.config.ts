f800738bab9d1ca8b4405350b3332c9bf4eda24a
[dyad] <huulong111@gmail.com>
2025-09-29 01:19:27 +0700

    [dyad] Thay đổi font chữ ứng dụng - wrote 3 file(s) + extra files edited outside of Dyad

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ["Inter", ...fontFamily.sans],
+        sans: ["Be Vietnam Pro", ...fontFamily.sans],
       },
       colors: {
         border: "hsl(var(--border))",
